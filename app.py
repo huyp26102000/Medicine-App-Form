a16bdbaca77edaab4cbbdbd0f4e24a9e25cc188f
huyp26102000 <huyp26102000@gmail.com>
2021-04-20 17:46:06 +0700

    update Medicine updatation

--- app.py
+++ app.py
@@ -9,25 +9,35 @@ app = Flask(__name__)
 @app.route('/')
 
 def welcome():
-    return redirect('/showForm')
+    return redirect('/login')
 @app.route('/showForm', methods=['GET', 'POST'])
 def showForm():
-    tmp = getBox1Infor()
-    tmpdate = 'Ngày ' + tmp['date'].strftime("%d") + ' Tháng ' + tmp['date'].strftime("%m") + ' Năm 20' + tmp['date'].strftime("%y")
+    session['NewestMed'] = get_Newest_Med_Infor(session.get('identity')['ID'])
+    session['tmpDate'] = 'Ngày ' + session.get('NewestMed')['date'].strftime("%d") + ' Tháng ' + session.get('NewestMed')['date'].strftime("%m") + ' Năm 20' + session.get('NewestMed')['date'].strftime("%y")
+    session['MedWhenGood'] = getMed_WhenGood(session.get('NewestMed')['MedID'])
     if request.method == 'POST':
         return redirect(url_for('login')) 
     else:
-        return render_template( 'ShowForm.html',
-                                    fullname = tmp['fullname'],
-                                    Doctor = tmp['Doctor'],
-                                    Date = tmpdate)
+        return render_template('ShowForm.html',
+                                fullname = session.get('NewestMed')['fullname'],
+                                Doctor = session.get('NewestMed')['Doctor'],
+                                Date = session.get('tmpDate'),
+                                daily_Med_1 = session.get('MedWhenGood')['daily_Med_1'],
+                                daily_Med_Num_1 = session.get('MedWhenGood')['daily_Med_Num_1'],
+                                daily_Med_use_1 = session.get('MedWhenGood')['daily_Med_use_1'],
+                                daily_Med_2 = session.get('MedWhenGood')['daily_Med_2'],
+                                daily_Med_Num_2 = session.get('MedWhenGood')['daily_Med_Num_2'],
+                                daily_Med_use_2 = session.get('MedWhenGood')['daily_Med_use_2'],
+                                reliever_Med = session.get('MedWhenGood')['reliever_Med']
+                                )
+
  
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         session['logged_in'] = False
     if not session.get('logged_in'):
-        return redirect(url_for('showForm'))
+        return redirect(url_for('login'))
     else:
         return render_template('inputForm.html')
 @app.route('/register', methods=['GET', 'POST'])
@@ -40,18 +50,22 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    session['logged_in'] = False
     if request.method == 'POST':
-        if validLogin(request.form['username'], encryp(request.form['password'])) == False:
+        session['identity'] = validLogin(request.form['username'], encryp(request.form['password']))
+        if  session.get('identity') == None:
             error = 'Invalid Credentials. Please try again.'
             session['logged_in'] = False
-
         else:
             session['logged_in'] = True
         # print(ip_address)
     if not session.get('logged_in'):
         return render_template('loginV2.html', error=error)
     else:
-        return redirect(url_for('home'))
+        if session.get('identity')['role'] == '0':
+            return redirect(url_for('home'))
+        if session.get('identity')['role'] == '1':
+            return redirect(url_for('showForm'))
     
  
  
--- function.py
+++ function.py
@@ -3,51 +3,117 @@ from firebase_admin import credentials, firestore
 import hashlib
 from model import *
 from datetime import datetime
+import uuid
 
 cred = credentials.Certificate("./inspiration/serviceAccountKey.json")
 default_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+def getNewID():
+    id = uuid.uuid4()
+    return str(id)
 def validLogin(ID, Password):
     docs = db.collection(u'Account').stream()
-    for doc in docs:
+    allDoc = list(docs)
+    for doc in allDoc:
         if ID == doc.to_dict()['username'] and Password == doc.to_dict()['Password']:
-                print("true")
-                return True
-    return False
-def getBox1Infor():
-    docs = db.collection(u'GeneralInfor').stream()
+                return doc.to_dict()
+    return None
+def get_Newest_Med_Infor(PatientID):
+    docs = db.collection(u'Med_Infor').stream()
     allDoc = list(docs)
     for i in range(0, len(allDoc)):
         allDoc[i] = allDoc[i].to_dict()
         allDoc[i]['date'] = datetime.strptime(allDoc[i]['date'], '%d/%m/%y')
     tmp = allDoc[0]
     for i in range(1, len(allDoc)):
-        if(tmp['date'] < allDoc[i]['date']):
-            tmp = allDoc[i]
+        if(tmp['date'] < allDoc[i]['date'] and tmp['ID']==PatientID):
+            tmp = sortPatient[i]
     return tmp
-
-    # print(allDoc)
 def encryp(string_to_hash):
     hash_object = hashlib.sha256(str(string_to_hash).encode('utf-8'))
     return hash_object.hexdigest()
-def submidGeneralInfor(ID, fullName, Doctor, date):
+def submit_NewMed_Infor(ID, fullName, Doctor, date):
+    MedID = getNewID()
     data = {
         u'ID': ID,
+        u'MedID': MedID,
         u'fullname': fullName,
         u'Doctor': Doctor,
         u'date': date
         }
-    db.collection(u'GeneralInfor').document().set(data)
-def submidGeneralInfor(ID, fullName, Doctor, date):
+    db.collection(u'Med_Infor').document().set(data)
+    return MedID
+def submidWhenGood( MedID,
+                    daily_Med_1,
+                    daily_Med_Num_1,
+                    daily_Med_use_1,
+                    daily_Med_2,
+                    daily_Med_Num_2,
+                    daily_Med_use_2,
+                    reliever_Med):
     data = {
-        u'ID': ID,
-        u'fullname': fullName,
-        u'Doctor': Doctor,
-        u'date': date
+        u'MedID': MedID,
+        u'daily_Med_1': daily_Med_1,
+        u'daily_Med_Num_1': daily_Med_Num_1,
+        u'daily_Med_use_1': daily_Med_use_1,
+        u'daily_Med_2': daily_Med_2,
+        u'daily_Med_Num_2': daily_Med_Num_2,
+        u'daily_Med_use_2': daily_Med_use_2,
+        u'reliever_Med': reliever_Med
+        }
+    db.collection(u'Medicine_WhenGood').document().set(data)
+def getMed_WhenGood(MedID):
+    docs = db.collection(u'Medicine_WhenGood').stream()
+    allDoc = list(docs)
+    for i in range(0, len(allDoc)):
+        allDoc[i] = allDoc[i].to_dict()
+    for tmp in allDoc:
+        if(tmp['MedID']==MedID): return(tmp)
+    return None
+def submidWhenNotgood(  MedID,
+                        addMed,
+                        addMed_Num,
+                        addMed_use):
+    data = {
+        u'MedID': MedID,
+        u'addMed': addMed,
+        u'addMed_Num': addMed_Num,
+        u'addMed_use': addMed_use
+        }
+    db.collection(u'Medicine_WhenNotGood').document().set(data)
+# def getMed_WhenNotgood(MedID):
+#     docs = db.collection(u'Medicine_WhenGood').stream()
+#     allDoc = list(docs)
+#     for i in range(0, len(allDoc)):
+#         allDoc[i] = allDoc[i].to_dict()
+#     for tmp in allDoc:
+#         if(tmp['MedID']==MedID): return(tmp)
+#     return None
+def submidWhenBad(  MedID,
+                    Prednisone_time,
+                    Prednisone_num,
+                    Prednisone_days,
+                    Methylprednisolon_time,
+                    Methylprednisolon_num,
+                    Methylprednisolon_days,
+                    ):
+    data = {
+        u'MedID': MedID,
+        u'Prednisone_time': Prednisone_time,
+        u'Prednisone_num': Prednisone_num,
+        u'Prednisone_days': Prednisone_days,
+        u'Methylprednisolon_time': Methylprednisolon_time,
+        u'Methylprednisolon_num': Methylprednisolon_num,
+        u'Methylprednisolon_days': Methylprednisolon_days,
         }
     db.collection(u'GeneralInfor').document().set(data)
-# def granting(ID, Password):
 
-# submidGeneralInfor('asdasdasdasd', 'demoName', 'demo2', '1-Nov-18')
-# getBox1Infor()
+
+# print(getMed_WhenGood(get_Newest_Med_Infor('asdasdasdasd')['MedID']))
+# submidWhenGood(submit_NewMed_Infor('asdasdasdasd', 'Hoang', 'demo2', '8/9/20'),
+#                 '3','5','3','2','2','2','dbcsssdd')
+# submidWhenNotgood(submit_NewMed_Infor('8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Hoang', 'demo2', '8/9/20'),
+#                 'yyyy','5','3')
+# submidWhenNotgood('')
+# getMed_WhenGood('abc')
